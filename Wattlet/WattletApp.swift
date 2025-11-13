abba81cdb7a52ec5b2640b53897c2be8847f5c29
PaoloAC <paolocorreyero@gmail.com>
2025-11-13 12:40:23 +0100

    Initial Commit

---
+++ Wattlet/WattletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattletApp.swift
+//  Wattlet
+//
+//  Created by Paolo AC on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WattletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WattletTests/WattletTests.swift
@@ -0,0 +1,16 @@
+//
+//  WattletTests.swift
+//  WattletTests
+//
+//  Created by Paolo AC on 13/11/25.
+//
+
+import Testing
+
+struct WattletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
